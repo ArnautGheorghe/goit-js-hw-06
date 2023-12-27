bd1555d03a56e029429f8e7188a87e480d18cf93
ArnautGheorghe <agigi94@yahoo.com>
2023-12-27 13:50:05 +0200

    exercitiul 2

--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -6,3 +6,10 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list = document.querySelector("#ingredients");
+for (let i = 0; i < ingredients.length; i++) {
+  const lastItem = document.createElement("li");
+  lastItem.textContent = ingredients[i];
+  list.append(lastItem);
+  console.log(lastItem.innerHTML);
+}
