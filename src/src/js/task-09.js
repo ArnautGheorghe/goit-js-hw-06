48ddeb3dc9f2d94cf3baea3fc3fbe82b3f1c56e7
ArnautGheorghe <agigi94@yahoo.com>
2023-12-28 15:40:50 +0200

    exercitiul 9

--- src/src/js/task-09.js
+++ src/src/js/task-09.js
@@ -1,5 +1,25 @@
 function getRandomHexColor() {
+  
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+const fundal = document.querySelector(".widget");
+const butonel = document.querySelector(".change-color");
+
+fundal.style.backgroundColor = "none";
+
+
+butonel.addEventListener("click", () => {
+  fundal.style.backgroundColor = getRandomHexColor();
+  let modifColor = document.querySelector(".color");
+modifColor.innerHTML = `<span >#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}</span> `;
+  console.log(
+    `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`
+  );
+});
+
