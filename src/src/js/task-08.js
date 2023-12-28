f6271ae89ec43eb276265a4e8ef7b9656ab3437e
ArnautGheorghe <agigi94@yahoo.com>
2023-12-28 15:06:06 +0200

    exercitiul 8

--- src/src/js/task-08.js
+++ src/src/js/task-08.js
@@ -1,1 +1,19 @@
+const submitForm = document.querySelector(".login-form");
 
+submitForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+  
+    if (email === "" || password === "") {
+      alert("Please fill in all the fields!");
+    return console.log("Please fill in all the fields!");
+  }
+
+    console.log(`Email: ${email}, Password: ${password}`);
+    
+  form.reset();
+}
